5e24320ffddd9dadaad847488e852e26725140c3
Xing <x.xing.unsw@gmail.com>
2020-12-19 10:58:54 +1100

    init

---
+++ BirthdayCakeCandles/BirthdayCakeCandles/BirthdayCakeCandles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BirthdayCakeCandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            if (candles.Count == 0) return 0;
+            var dict = new Dictionary<int, int>();
+            var highest = candles[0];
+            foreach (var candle in candles)
+            {
+                highest = Math.Max(highest, candle);
+                if (!dict.ContainsKey(candle))
+                    dict[candle] = 0;
+                dict[candle]++;
+            }
+            return dict[highest];
+        }
+    }
+}
