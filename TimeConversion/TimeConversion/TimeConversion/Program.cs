97e1150e3925e7a0bc8b4ce501304a9b96534774
Xing <x.xing.unsw@gmail.com>
2020-12-19 11:10:11 +1100

    init

---
+++ TimeConversion/TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static string timeConversion(string s)
+        {
+            if (s.Substring(s.Length - 2) == "AM")
+                return s.Substring(0, 2) == "12" ? "00" + s.Substring(2, s.Length - 4) : s.Substring(0, s.Length - 2);
+            return s.Substring(0, 2) == "12"
+                ? s.Substring(0, s.Length - 2)
+                : (int.Parse(s.Substring(0, 2)) + 12) + s.Substring(2, s.Length - 4);
+        }
+    }
+}
