77b5160a8fbd95aeca83f1a2d011809484a062c4
Xing <x.xing.unsw@gmail.com>
2020-12-22 21:21:49 +1100

    init

---
+++ AppleAndOrange/AppleAndOrange/AppleAndOrange/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace AppleAndOrange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void CountApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+            int appleCount = apples.Count(apple => a + apple >= s && a + apple <= t);
+            int orangeCount = oranges.Count(orange => b + orange >= s && b + orange <= t);
+            Console.WriteLine(appleCount);
+            Console.WriteLine(orangeCount);
+        }
+    }
+}
